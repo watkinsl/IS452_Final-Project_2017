3a4f450da508b5523e0a492fa445a9d67cdf7d72
Lisa Marie <watkinsl@illinois.edu>
2017-12-14 22:32:39 -0600

    Looping through multiple artist files
    Substantial work on developing a program that loops through various web files of mutiple artists and extracts the necessary information. The only one left is extracting # of Instagram followers.

--- watkinsl_IS452_final-project.py
+++ watkinsl_IS452_final-project.py
@@ -13,103 +13,131 @@ from bs4 import BeautifulSoup
 # CREATING PROGRAM
 # ------------------------
 
-# filenames = ['artist_Bruno-Mars.html', 'IG_Bruno-Mars.htm', 'Twitter_Bruno-Mars.htm', 'artist_Drake.html',
-#             'IG_Drake.htm', 'Twitter_Drake.htm']
-#
-# artist_rows = []
-#
-# for file_path in filenames:
-#     with open(file_path, 'r') as f_input:
-#         read_in = f_input.read()
-#         soup = BeautifulSoup(read_in, 'html.parser')
-#         if file_path.startswith('artist'):
-#             artist_name = soup.find('div', attrs={'class': 'artistDetails'}) # line 4230
-#             name = artist_name.find('h1')
-#             only_name = name.text.strip() # strip() is used to remove starting and trailing
-#             artist_rows.append(only_name)
-#         if file_path.startswith('IG'):
-#             IG_followers = soup.find('span', attrs={'title': '15,721,693'})
-#             IG_followers_text = IG_followers.text.strip()
-#             print(IG_followers_text)
-# print(artist_rows)
+filenames = ['artist_Bruno-Mars.html', 'IG_Bruno-Mars.htm', 'Twitter_Bruno-Mars.htm', 'artist_Drake.html',
+            'IG_Drake.htm', 'Twitter_Drake.htm']
+
+artist_rows = []
+
+for file_path in filenames:
+    with open(file_path, 'r') as f_input:
+        read_in = f_input.read()
+        soup = BeautifulSoup(read_in, 'html.parser')
+        if file_path.startswith('artist'):
+            #artist's name
+            artist_name = soup.find('div', attrs={'class': 'artistDetails'}) # line 4230
+            name = artist_name.find('h1')
+            only_name = name.text.strip() # strip() is used to remove starting and trailing
+
+            # rating on Ticketmaster
+            artist_rating = soup.find('span', attrs={'itemprop': 'ratingValue'}) # line 4333
+            rating_text = artist_rating.text.strip()
+
+            # no. of Ticketmaster reviews
+            artist_reviews = soup.find('span', attrs={'class': 'BVRRCount BVRRNonZeroCount'}) # line 4333
+            artist_reviews_num = artist_reviews.find('span', attrs={'class': 'BVRRNumber'})
+            reviews_text = artist_reviews_num.text.strip()
+
+            artist_rows.extend([only_name, rating_text, reviews_text])
+
+        elif file_path.startswith('IG'):
+            # no. of Instagram posts
+            IG_posts = soup.find('span', attrs={'class': '_fd86t'})
+            #IG_followers_num = IG_followers.find('title')
+            IG_post_num = IG_posts.text.strip()
+            artist_rows.append(IG_post_num)
+
+        elif file_path.startswith('Twitter'):
+            # no. of tweets
+            tweets = soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--tweets is-active'})
+            tweets_text = tweets.find('span', attrs={'class': 'ProfileNav-value'})
+            tweets_num = tweets_text.text.strip()
+
+            # no. of Twitter followers
+            Twitter_followers = soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--followers'})
+            Twitter_followers_text = Twitter_followers.find('span', attrs={'class': 'ProfileNav-value'})
+            Twitter_followers_num = Twitter_followers_text.text.strip()
+
+            artist_rows.extend([tweets_num, Twitter_followers_num])
+
+print(artist_rows)
 
 # ------------------------
 # WORKING CODE
 # ------------------------
 
-filein = open('artist_Bruno-Mars.html', 'r')
-insta_in = open('IG_Bruno-Mars.htm', 'r')
-twitter_in = open('Twitter_Bruno-Mars.htm', 'r')
-
-bruno_page = filein.read()
-insta_bruno = insta_in.read()
-twitter_bruno = twitter_in.read()
-
-filein.close()
-insta_in.close()
-twitter_in.close()
-
-artists_list = []
-
-# parse the html using beautiful soap and store in variable `soup`
-soup = BeautifulSoup(bruno_page, 'html.parser')
-IG_soup = BeautifulSoup(insta_bruno, 'html.parser')
-twitter_soup = BeautifulSoup(twitter_bruno, 'html.parser')
-
-# EXTRACTING ARTIST'S NAME
-artist_name = soup.find('div', attrs={'class': 'artistDetails'}) # line 4230
-name = artist_name.find('h1')
-only_name = name.text.strip() # strip() is used to remove starting and trailing
-artists_list.append(only_name.strip("\xa0Tickets"))
-
-# FINDING TICKETMASTER RATING FOR ARTIST
-artist_rating = soup.find('span', attrs={'itemprop': 'ratingValue'}) # line 4333
-rating_text = artist_rating.text.strip()
-artists_list.append(rating_text)
-
-# FINDING NUMBER OF REVIEWS ON ARTIST VIA TICKETMASTER
-artist_reviews = soup.find('span', attrs={'class': 'BVRRCount BVRRNonZeroCount'}) # line 4333
-artist_reviews_num = artist_reviews.find('span', attrs={'class': 'BVRRNumber'})
-reviews_text = artist_reviews_num.text.strip()
-#print("Bruno Mars has", reviews_text, "overall on Ticketmaster.")
-artists_list.append(reviews_text)
-
-# RATING BREAKDOWN ON TICKETMASTER
-artist_reviews_breakdown = soup.find('div', attrs={'class': 'BVRRHistogramContent'}) # line 4333
-reviews_breakdown_text = artist_reviews_breakdown.text.strip()
-artists_list.append(reviews_breakdown_text)
-
-# ------------------------
-
-# NO. OF INSTAGRAM FOLLOWERS
-IG_followers = IG_soup.find('span', attrs={'title': '15,721,693'})
-IG_followers_text = IG_followers.text.strip()
-#print("Bruno's Insta has", IG_followers_text, "followers.")
-artists_list.append(IG_followers_text)
-
-# # NO. OF INSTAGRAM POSTS
-IG_posts = IG_soup.find('span', attrs={'class': '_fd86t'})
-IG_posts_text = IG_posts.text.strip()
-#print("Bruno's Insta has", IG_posts_text, "posts.")
-artists_list.append(IG_posts_text)
-
-# ------------------------
-# NUMBER OF TWITTER FOLLOWERS
-Twitter_followers = twitter_soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--followers'})
-Twitter_followers_text = Twitter_followers.find('span', attrs={'class': 'ProfileNav-value'})
-Twitter_followers_num = Twitter_followers_text.text.strip()
-#print("Bruno's Twitter has", Twitter_followers_num, "followers.")
-artists_list.append(Twitter_followers_num)
-
-# NUMBER OF TWEETS
-tweets = twitter_soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--tweets is-active'})
-tweets_text = tweets.find('span', attrs={'class': 'ProfileNav-value'})
-tweets_num = tweets_text.text.strip()
-#print("Bruno's Twitter has", tweets_num, "tweets.")
-artists_list.append(tweets_num)
-
-# need to create a tuple for the row contents
-print(artists_list)
+# filein = open('artist_Bruno-Mars.html', 'r')
+# insta_in = open('IG_Bruno-Mars.htm', 'r')
+# twitter_in = open('Twitter_Bruno-Mars.htm', 'r')
+#
+# bruno_page = filein.read()
+# insta_bruno = insta_in.read()
+# twitter_bruno = twitter_in.read()
+#
+# filein.close()
+# insta_in.close()
+# twitter_in.close()
+#
+# artists_list = []
+#
+# # parse the html using beautiful soap and store in variable `soup`
+# soup = BeautifulSoup(bruno_page, 'html.parser')
+# IG_soup = BeautifulSoup(insta_bruno, 'html.parser')
+# twitter_soup = BeautifulSoup(twitter_bruno, 'html.parser')
+#
+# # EXTRACTING ARTIST'S NAME
+# artist_name = soup.find('div', attrs={'class': 'artistDetails'}) # line 4230
+# name = artist_name.find('h1')
+# only_name = name.text.strip() # strip() is used to remove starting and trailing
+# artists_list.append(only_name.strip("\xa0Tickets"))
+#
+# # FINDING TICKETMASTER RATING FOR ARTIST
+# artist_rating = soup.find('span', attrs={'itemprop': 'ratingValue'}) # line 4333
+# rating_text = artist_rating.text.strip()
+# artists_list.append(rating_text)
+#
+# # FINDING NUMBER OF REVIEWS ON ARTIST VIA TICKETMASTER
+# artist_reviews = soup.find('span', attrs={'class': 'BVRRCount BVRRNonZeroCount'}) # line 4333
+# artist_reviews_num = artist_reviews.find('span', attrs={'class': 'BVRRNumber'})
+# reviews_text = artist_reviews_num.text.strip()
+# #print("Bruno Mars has", reviews_text, "overall on Ticketmaster.")
+# artists_list.append(reviews_text)
+#
+# # RATING BREAKDOWN ON TICKETMASTER
+# artist_reviews_breakdown = soup.find('div', attrs={'class': 'BVRRHistogramContent'}) # line 4333
+# reviews_breakdown_text = artist_reviews_breakdown.text.strip()
+# artists_list.append(reviews_breakdown_text)
+#
+# # ------------------------
+#
+# # NO. OF INSTAGRAM FOLLOWERS
+# IG_followers = IG_soup.find('span', attrs={'title': '15,721,693'})
+# IG_followers_text = IG_followers.text.strip()
+# #print("Bruno's Insta has", IG_followers_text, "followers.")
+# artists_list.append(IG_followers_text)
+#
+# # # NO. OF INSTAGRAM POSTS
+# IG_posts = IG_soup.find('span', attrs={'class': '_fd86t'})
+# IG_posts_text = IG_posts.text.strip()
+# #print("Bruno's Insta has", IG_posts_text, "posts.")
+# artists_list.append(IG_posts_text)
+#
+# # ------------------------
+# # NUMBER OF TWITTER FOLLOWERS
+# Twitter_followers = twitter_soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--followers'})
+# Twitter_followers_text = Twitter_followers.find('span', attrs={'class': 'ProfileNav-value'})
+# Twitter_followers_num = Twitter_followers_text.text.strip()
+# #print("Bruno's Twitter has", Twitter_followers_num, "followers.")
+# artists_list.append(Twitter_followers_num)
+#
+# # NUMBER OF TWEETS
+# tweets = twitter_soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--tweets is-active'})
+# tweets_text = tweets.find('span', attrs={'class': 'ProfileNav-value'})
+# tweets_num = tweets_text.text.strip()
+# #print("Bruno's Twitter has", tweets_num, "tweets.")
+# artists_list.append(tweets_num)
+#
+# # need to create a tuple for the row contents
+# print(artists_list)
 
 
 # ------------------------
