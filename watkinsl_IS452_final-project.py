6e0f293f0b3248410b75bc5f68fcf53a1cb54d11
Lisa Marie <watkinsl@illinois.edu>
2017-12-16 16:40:39 -0600

    New webfiles & completed code
    Adding new necessary web files for each of the remaining artists; completed code (it works, yay!); and the CSV file that is output when the program runs.

--- watkinsl_IS452_final-project.py
+++ watkinsl_IS452_final-project.py
@@ -10,94 +10,119 @@
 # [x] Be able to extract data from webpages
 # [x] Be able to write to a CSV file in correct rows & columns
 # [x] Combine tasks into a loop that will go over multiple files
-# [] Save out results to a nested list that will be used for the CSV file
-# [don't do] Use pandas to create a visualization based on the data saved to the CSV file
-# [don't do] Export and post on a website (?)
-# - OR - [don't do] Be able to create different visualizations to figure out different aspects of the data
+# [x] Save out results to a nested list that will be used for the CSV file
+# [don't need] Use pandas to create a visualization based on the data saved to the CSV file
+# [don't need] Export and post on a website (?)
+# - OR - [don't need] Be able to create different visualizations to figure out different aspects of the data
 # [x] create function split on period to evaluate the millions into an actual number (i.e. 38.9M) * find package
-# [] accumulator to create list of lists (xpathb homework to make lists of lists)
+# [x] accumulator to create list of lists (xpathb homework to make lists of lists)
 # ------------------------
 
 # import libraries
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from lxml import html
+import csv
 
 # ------------------------
-# CREATING ACTUAL PROGRAM
+# ACTUAL PROGRAM
 # ------------------------
 # define function for convert strings of numbers into integers
 
 
 def convert_num(num):
-    mil_num = num.split('M')
-    mil_num_float = float(mil_num[0])
-    m_to_num = int(mil_num_float * 1000000)
+    if num.endswith('M'):
+        mil_num = num.split('M')
+        mil_num_float = float(mil_num[0])
+        m_to_num = int(mil_num_float * 1000000)
+        return m_to_num
 
-    return m_to_num
+    elif num.endswith('K'):
+        k_num = num.split('K')
+        k_num_float = float(k_num[0])
+        k_to_num = int(k_num_float * 1000)
+        return k_to_num
+
+    else:
+        int_num = int(num.replace(',', ''))
+        return int_num
 
 
-filenames = ['artist_Bruno-Mars.html', 'IG_Bruno-Mars.htm', 'Twitter_Bruno-Mars.htm', 'artist_Drake.html',
-            'IG_Drake.htm', 'Twitter_Drake.htm']
+filenames = [['artist_Bruno-Mars.html', 'IG_Bruno-Mars.htm', 'Twitter_Bruno-Mars.htm'], ['artist_Drake.html',
+            'IG_Drake.htm', 'Twitter_Drake.htm'], ['artist_Kendrick-Lamar.html', 'IG_Kendrick-Lamar.htm',
+            'Twitter_Kendrick-Lamar.htm'], ['artist_Ed-Sheeran.html', 'IG_Ed-Sheeran.htm', 'Twitter_Ed-Sheeran.htm'],
+             ['artist_The-Chainsmokers.html', 'IG_The-Chainsmokers.htm', 'Twitter_The-Chainsmokers.htm']]
 
 artist_rows = []
 
-#artist_dict = {'Bruno Mars', 'Drake', 'The Chainsmokers', 'Kendrick Lamar', 'Ed Sheeran'}
-
-for file_path in filenames:
+for list_of_files in filenames:
     artist_data = []
-    with open(file_path, 'r') as f_input:
-        read_in = f_input.read()
-        soup = BeautifulSoup(read_in, 'html.parser')
-        if file_path.startswith('artist'):
-            #artist's name
-            artist_name = soup.find('div', attrs={'class': 'artistDetails'}) # line 4230
-            name = artist_name.find('h1')
-            only_name = name.text.strip() # strip() is used to remove starting and trailing
-            #print(only_name.split('\xa0Tickets')[0])
-            # rating on Ticketmaster
-            artist_rating = soup.find('span', attrs={'itemprop': 'ratingValue'}) # line 4333
-            rating_text = artist_rating.text.strip()
+    for file_path in list_of_files:
+        with open(file_path, 'r') as f_input:
+            read_in = f_input.read()
+            soup = BeautifulSoup(read_in, 'html.parser')
+            if file_path.startswith('artist'):
+                # artist's name
+                artist_name = soup.find('div', attrs={'class': 'artistDetails'}) # line 4230
+                name = artist_name.find('h1')
+                only_name = name.text.strip() # strip() is used to remove starting and trailing
 
-            # no. of Ticketmaster reviews
-            artist_reviews = soup.find('span', attrs={'class': 'BVRRCount BVRRNonZeroCount'}) # line 4333
-            artist_reviews_num = artist_reviews.find('span', attrs={'class': 'BVRRNumber'})
-            reviews_text = artist_reviews_num.text.strip()
+                # rating on Ticketmaster
+                artist_rating = soup.find('span', attrs={'itemprop': 'ratingValue'}) # line 4333
+                rating_text = artist_rating.text.strip()
 
-            artist_data.extend([only_name.split('\xa0Tickets')[0], float(rating_text), int(reviews_text.replace(',', ''))])
+                # no. of Ticketmaster reviews
+                artist_reviews = soup.find('span', attrs={'class': 'BVRRCount BVRRNonZeroCount'}) # line 4333
+                artist_reviews_num = artist_reviews.find('span', attrs={'class': 'BVRRNumber'})
+                reviews_text = artist_reviews_num.text.strip()
 
-        elif file_path.startswith('IG'):
-            #page = soup
-            tree = html.fromstring(read_in)
+                artist_data.extend([only_name.split('\xa0Tickets')[0], float(rating_text), int(reviews_text.replace(',', ''))])
 
-            # no. of Instagram posts
-            IG_posts = soup.find('span', attrs={'class': '_fd86t'})
-            #IG_followers_num = IG_followers.find('title')
-            IG_post_num = IG_posts.text.strip()
+            elif file_path.startswith('IG'):
+                tree = html.fromstring(read_in)
 
-            # no. of Instagram followers (needs work)
-            IG_followers = tree.xpath('//span[@class="_fd86t"]/@title')
-            # refer to assignment to get rid of xpath list
-            artist_data.extend([int(IG_post_num.replace(',', '')), int(IG_followers[0].replace(',', ''))])
+                # no. of Instagram posts
+                IG_posts = soup.find('span', attrs={'class': '_fd86t'})
+                IG_post_num = IG_posts.text.strip()
 
-            #print(IG_followers)
+                # no. of Instagram followers (needs work)
+                IG_followers = tree.xpath('//span[@class="_fd86t"]/@title')
+                # refer to assignment to get rid of xpath list
+                artist_data.extend([int(IG_post_num.replace(',', '')), int(IG_followers[0].replace(',', ''))])
 
-        elif file_path.startswith('Twitter'):
-            # no. of tweets
-            tweets = soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--tweets is-active'})
-            tweets_text = tweets.find('span', attrs={'class': 'ProfileNav-value'})
-            tweets_num = tweets_text.text.strip()
+            elif file_path.startswith('Twitter'):
+                # no. of tweets
+                tweets = soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--tweets is-active'})
+                tweets_text = tweets.find('span', attrs={'class': 'ProfileNav-value'})
+                tweets_num = tweets_text.text.strip()
+                tweets_convert = convert_num(tweets_num)
 
-            # no. of Twitter followers
-            Twitter_followers = soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--followers'})
-            Twitter_followers_text = Twitter_followers.find('span', attrs={'class': 'ProfileNav-value'})
-            Twitter_followers_num = Twitter_followers_text.text.strip()
-            Twit_num = convert_num(Twitter_followers_num)
+                # no. of Twitter followers
+                Twitter_followers = soup.find('li', attrs={'class': 'ProfileNav-item ProfileNav-item--followers'})
+                Twitter_followers_text = Twitter_followers.find('span', attrs={'class': 'ProfileNav-value'})
+                Twitter_followers_num = Twitter_followers_text.text.strip()
+                Twit_num = convert_num(Twitter_followers_num)
 
-            artist_data.extend([int(tweets_num.replace(',', '')), Twit_num])
+                # artist_data.extend([int(tweets_num.replace(',', '')), Twit_num])
+                artist_data.extend([tweets_convert, Twit_num])
+
     artist_rows.append(artist_data)
 
 print(artist_rows)
+
+# ------------------------
+# EXPORTING A CSV FILE [x]
+# ------------------------
+
+headers = ['Artist Name', 'Ticketmaster (TM) Rating', '# of TM Reviews', '# of Instagram (IG) Posts',
+           '# of IG Followers', '# of Tweets', '# of Twitter Followers']
+
+outfile = open('artists.csv', 'w')
+csv_out = csv.writer(outfile)
+csv_out.writerow(headers)
+csv_out.writerows(artist_rows)
+outfile.close()
+
 
 # ------------------------
 # WORKING CODE
@@ -176,31 +201,6 @@ print(artist_rows)
 #
 # # need to create a tuple for the row contents
 # print(artists_list)
-
-
-# ------------------------
-# EXPORTING A CSV FILE [x]
-# ------------------------
-
-# Now, use the CSV module to write out this data.
-# Let the header values be:  id, status, occupation
-# Let the file name be hamlet-character-data.csv
-# You must use the CSV module to receive full credit
-# This should yield a csv file with 3 columns and 34 rows.
-
-
-# Here's a template. (Source:  http://chimera.labs.oreilly.com/books/1230000000393/ch06.html#to_write_csv_da)
-# import csv
-#
-# headers = ['Artist Name', 'Ticketmaster (TM) Rating', '# of TM Reviews', 'TM Review Breakdown', '# of Instagram (IG) Followers',
-#            '# of IG Posts', '# of Twitter Followers', '# of Tweets']
-#
-# outfile = open('bruno-mars.csv', 'w')
-# csv_out = csv.writer(outfile)
-# csv_out.writerow(headers) # add your
-# csv_out.writerow([only_name, rating_text, reviews_text, reviews_breakdown_text, IG_followers_text, IG_posts_text,
-#                    Twitter_followers_num, tweets_num])
-# outfile.close()
 
 # ------------------------
 # OLD CODE
